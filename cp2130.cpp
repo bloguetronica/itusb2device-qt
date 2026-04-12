c5a1875fd9a558701dc36a9048dfaa06460c0669
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2026-04-12 23:02:18 +0100

    Add files via upload

--- cp2130.cpp
+++ cp2130.cpp
@@ -1,5 +1,5 @@
-/* CP2130 class for Qt - Version 2.2.6
-   Copyright (c) 2021-2024 Samuel Lourenço
+/* CP2130 class for Qt - Version 2.2.7
+   Copyright (c) 2021-2026 Samuel Lourenço
 
    This library is free software: you can redistribute it and/or modify it
    under the terms of the GNU Lesser General Public License as published by
@@ -26,6 +26,7 @@ extern "C" {
 }
 
 // Definitions
+const int IFACE = 0;                  // Interface number (implemented in version 2.2.7)
 const unsigned int TR_TIMEOUT = 500;  // Transfer timeout in milliseconds (increased to 500ms since version 2.0.2)
 
 // Specific to getDescGeneric() and writeDescGeneric() (added in version 2.1.0)
@@ -245,9 +246,9 @@ void CP2130::bulkTransfer(quint8 endpointAddr, unsigned char *data, int length, 
 void CP2130::close()
 {
     if (isOpen()) {  // This condition avoids a segmentation fault if the calling algorithm tries, for some reason, to close the same device twice (e.g., if the device is already closed when the destructor is called)
-        libusb_release_interface(handle_, 0);  // Release the interface
+        libusb_release_interface(handle_, IFACE);  // Release the interface (modified in version 2.2.7)
         if (kernelWasAttached_) {  // If a kernel driver was attached to the interface before
-            libusb_attach_kernel_driver(handle_, 0);  // Reattach the kernel driver
+            libusb_attach_kernel_driver(handle_, IFACE);  // Reattach the kernel driver (modified in version 2.2.7)
         }
         libusb_close(handle_);  // Close the device
         libusb_exit(context_);  // Deinitialize libusb
@@ -671,7 +672,7 @@ void CP2130::lockOTP(int &errcnt, QString &errstr)
 int CP2130::open(quint16 vid, quint16 pid, const QString &serial)
 {
     int retval;
-    if (isOpen()) {  // Just in case the calling algorithm tries to open a device that was already sucessfully open, or tries to open different devices concurrently, all while using (or referencing to) the same object
+    if (isOpen()) {  // Just in case the calling algorithm tries to open a device that was already successfully open, or tries to open different devices concurrently, all while using (or referencing to) the same object
         retval = SUCCESS;
     } else if (libusb_init(&context_) != 0) {  // Initialize libusb. In case of failure
         retval = ERROR_INIT;
@@ -685,15 +686,15 @@ int CP2130::open(quint16 vid, quint16 pid, const QString &serial)
             libusb_exit(context_);  // Deinitialize libusb
             retval = ERROR_NOT_FOUND;
         } else {  // If the device is successfully opened and a handle obtained
-            if (libusb_kernel_driver_active(handle_, 0) == 1) {  // If a kernel driver is active on the interface
-                libusb_detach_kernel_driver(handle_, 0);  // Detach the kernel driver
+            if (libusb_kernel_driver_active(handle_, IFACE) == 1) {  // If a kernel driver is active on the interface (modified in version 2.2.7)
+                libusb_detach_kernel_driver(handle_, IFACE);  // Detach the kernel driver (modified in version 2.2.7)
                 kernelWasAttached_ = true;  // Flag that the kernel driver was attached
             } else {
                 kernelWasAttached_ = false;  // The kernel driver was not attached
             }
-            if (libusb_claim_interface(handle_, 0) != 0) {  // Claim the interface. In case of failure
+            if (libusb_claim_interface(handle_, IFACE) != 0) {  // Claim the interface. In case of failure (modified in version 2.2.7)
                 if (kernelWasAttached_) {  // If a kernel driver was attached to the interface before
-                    libusb_attach_kernel_driver(handle_, 0);  // Reattach the kernel driver
+                    libusb_attach_kernel_driver(handle_, IFACE);  // Reattach the kernel driver (modified in version 2.2.7)
                 }
                 libusb_close(handle_);  // Close the device
                 libusb_exit(context_);  // Deinitialize libusb
